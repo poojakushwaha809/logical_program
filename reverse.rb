260181e5e4ff6c2f3a62405870f3bdef8e73b460
poojakushwaha <poojakushwaha809@gmail.com>
2021-07-13 17:14:54 +0530

    Program to reverse a string without using String inbuilt function reverse()

---
+++ reverse.rb
@@ -0,0 +1,41 @@
+name = "pooja"
+b = name.split('')	
+c= name.size
+puts "before reverse = #{name}, after split =  #{b}, alphabet count = #{c}"
+
+arr = []
+(1..c).each do |d|
+	b.each_with_index do |a, i|
+		if i == c-d
+			arr.push(a)
+		end
+	end
+end
+ print "after reversed #{arr.join('')} \n"
+
+
+
+
+
+
+
+
+
+
+
+
+# 	b.each_with_index do |a, i|
+# 		puts "*******************here i #{a}******************** \n"
+
+# puts i 
+# (1..c).each do |d|
+# 	puts"************* here d************ \n"
+# 	puts d 
+# 	puts " ************* here 2nd  i*************\n"
+# 	puts i
+
+# 		if i == c-d
+# 			arr.push(a)
+# 		end
+# 	end
+# end 
